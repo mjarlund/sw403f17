abc560f0cac97a3d8c46822f7f28c965168b4915
mysjkin <iarogolo@gmail.com>
2017-03-06 08:32:54 +0100

    removed old ast files

--- P4test1/src/p4test/AST/Nodes/datatypes/Float.java
+++
@@ -1,9 +0,0 @@
-package p4test.AST.Nodes.datatypes;
-
-/**
- * Created by mysjkin on 3/3/17.
- */
-public class Float
-{
-
-}
--- P4test1/src/p4test/AST/Nodes/stmts/Until.java
+++
@@ -1,7 +0,0 @@
-package p4test.AST.Nodes.stmts;
-
-/**
- * Created by mysjkin on 3/4/17.
- */
-public class Until {
-}
