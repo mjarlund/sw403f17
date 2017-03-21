4ec2ee19625ea652c3ec4da2e9529ed0c56ae1f8
Drunar <andreascml@gmail.com>
2017-03-21 09:28:11 +0100

    further testing of scanner

--- P4test1/src/p4test/JUnit/SyntaxAnalysisTest/ScannerTest.java
+++ P4test1/src/p4test/JUnit/SyntaxAnalysisTest/ScannerTest.java
@@ -29,10 +29,53 @@ public class ScannerTest {
 	public static Collection<Object[]> generateData()
 	{
 		return Arrays.asList(new Object[][]{
+			//Operators
 			new Object[]{"or", TokenType.OPERATOR}, 
 			new Object[]{"and", TokenType.OPERATOR},
-			new Object[]{"jajajaj", TokenType.IDENTIFIER}
-			});
+			new Object[]{"equals", TokenType.OPERATOR},
+			new Object[]{"not", TokenType.OPERATOR},
+			new Object[]{"below", TokenType.OPERATOR},
+			new Object[]{"is", TokenType.OPERATOR},
+			new Object[]{"above", TokenType.OPERATOR},
+			
+			//Keywords
+			new Object[]{"until", TokenType.KEYWORD},
+			new Object[]{"if", TokenType.KEYWORD},
+			new Object[]{"string", TokenType.KEYWORD},
+			new Object[]{"return", TokenType.KEYWORD},
+			new Object[]{"structure", TokenType.KEYWORD},
+			new Object[]{"number", TokenType.KEYWORD},
+			new Object[]{"character", TokenType.KEYWORD},
+			new Object[]{"end", TokenType.KEYWORD},
+			new Object[]{"else", TokenType.KEYWORD},
+			new Object[]{"boolean", TokenType.KEYWORD},
+			new Object[]{"void", TokenType.KEYWORD},
+			new Object[]{"fraction", TokenType.KEYWORD},
+			new Object[]{"text", TokenType.KEYWORD},
+			
+			//Booleans
+			new Object[]{"true", TokenType.BOOLEAN_LITERAL},
+			new Object[]{"false", TokenType.BOOLEAN_LITERAL},
+			
+			//
+			
+			//Identifiers
+			new Object[]{"jajajaj", TokenType.IDENTIFIER},
+			new Object[]{"j", TokenType.IDENTIFIER},
+			//new Object[]{"", TokenType.IDENTIFIER},
+			
+			
+			//Integer Literals
+			new Object[]{"1", TokenType.INTEGER_LITERAL},
+			new Object[]{"-1", TokenType.INTEGER_LITERAL},
+			new Object[]{"0", TokenType.INTEGER_LITERAL},
+			new Object[]{"-999999999999999", TokenType.INTEGER_LITERAL},
+			new Object[]{"1000000000000000", TokenType.INTEGER_LITERAL},
+			new Object[]{"999abn", TokenType.INTEGER_LITERAL},
+			
+		
+		
+		});
 	}
 	
 	@Test
