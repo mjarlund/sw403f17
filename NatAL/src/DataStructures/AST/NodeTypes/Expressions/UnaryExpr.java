c5259d86b1d87c7ab54bc177f3a50a29b56a6e7d
mysjkin <iarogolo@gmail.com>
2017-03-21 15:02:50 +0100

    unary expr ast nodes

---
+++ NatAL/src/DataStructures/AST/NodeTypes/Expressions/UnaryExpr.java
@@ -0,0 +1,14 @@
+package DataStructures.AST.NodeTypes.Expressions;
+
+import DataStructures.AST.AST;
+import Syntax.Tokens.Token;
+
+/**
+ * Created by mysjkin on 3/21/17.
+ */
+public class UnaryExpr extends Expr
+{
+    public UnaryExpr(Token op, AST ValExpr)
+    {
+    }
+}
