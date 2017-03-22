7d0417cb56296174b738e5459300bf11f30aef82
mysjkin <iarogolo@gmail.com>
2017-03-22 08:27:44 +0100

    Very important stuff

--- NatAL/src/Semantics/Meme.java
+++ NatAL/src/Semantics/Meme.java
@@ -4,4 +4,12 @@ package Semantics;
  * Created by Anders Brams on 3/21/2017.
  */
 public class Meme {
+    // TODO RUN THIS VERY IMPORTANT MAIN
+    public static void main(String args[]) {
+        boolean andersIstUndFaglord = false;
+        String name = "Anders Brams";
+        if (name.equals("Anders Brams"))
+            andersIstUndFaglord = true;
+        System.out.println("Is Anders und faglord " + andersIstUndFaglord);
+    }
 }
