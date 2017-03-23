e781a14838a5023136ba6799186e50408f097cb9
Drunar <andreascml@gmail.com>
2017-03-23 12:27:39 +0100

    no message

--- NatAL/src/Test/UnitTests/SyntaxAnalysisTest/ParserTest/ParserTest.java
+++ NatAL/src/Test/UnitTests/SyntaxAnalysisTest/ParserTest/ParserTest.java
@@ -14,6 +14,7 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
 import Exceptions.MissingProductionsException;
+import Exceptions.UnexpectedTokenException;
 import Syntax.Parser.Parser;
 import Syntax.Scanner.Scanner;
 import Syntax.Tokens.TokenType;
@@ -79,7 +80,7 @@ public class ParserTest {
 	}
 	
 	
-	
+	/*
 	public static class multipleInputParserTest
 	{
 		@Test
@@ -97,6 +98,63 @@ public class ParserTest {
 			}			
 		}	
 	}
+	*/
+	@RunWith(Parameterized.class)
+	public static class AllFailableTests{
+		
+		private String testPath;
+
+		public AllFailableTests(String path)
+		{
+			this.testPath = path;
+		}		
+		
+		@Parameters(name = "{index}: successfullyFailedToPass({0})")
+		public static Collection<Object[]> generateData()
+		{
+			Collection<Object[]> data = new ArrayList<Object[]>();
+			
+			try{
+				File folder = new File("src/Test/TestPrograms/");
+				File[] listOfFiles = folder.listFiles();
+    		
+    			for (File file : listOfFiles) {
+    				if (file.isFile() && file.getName().contains("fail")) 
+    				{
+    					Object[] testFile = new Object[] { file.getPath() };
+    					data.add(testFile);
+    				}
+    			}
+			}
+			catch (Exception e){//Catch exception if any
+				  System.out.println("Error when reading files: " + e.getMessage());
+			}
+			return data;
+		}
+
+		@Test
+		public void ExceptionssuccessfullyThrownTest() throws IOException {
+			
+			boolean thrown = false;		
+			Scanner testScanner = new Scanner(InputTester.readFile(testPath));
+			try 
+			{
+				Parser parser = new Parser(testScanner);
+				parser.ParseProgram();
+			} 
+			catch (UnexpectedTokenException ex)
+			{					
+				thrown = true;
+			}			
+			catch (MissingProductionsException ex)
+			{
+				thrown = true;
+			}
+			assertEquals(true, thrown);
+		}		
+	}
+	
+	
 	
 	
 }
