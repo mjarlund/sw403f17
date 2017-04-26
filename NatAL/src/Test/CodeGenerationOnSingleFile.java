209a61f7a63569a5021d3476864f5517f4b59d0e
Drunar <andreascml@gmail.com>
2017-04-26 12:24:09 +0200

    no message

--- NatAL/src/Test/CodeGenerationOnSingleFile.java
+++ NatAL/src/Test/CodeGenerationOnSingleFile.java
@@ -11,7 +11,7 @@ import Syntax.Scanner.Scanner;
 public class CodeGenerationOnSingleFile {
 
 	public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(InputTester.readFile("src/Test/TestPrograms/semantics/4LayeredStructsTest"));
+        Scanner sc = new Scanner(InputTester.readFile("src/Test/TestPrograms/semantics/ForeachTest"));
         Parser parser = new Parser(sc);
         AST programTree = parser.ParseProgram();
         SemanticAnalyzer sm = new SemanticAnalyzer();
