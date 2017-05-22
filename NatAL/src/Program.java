48b8ab861c58b3d88c608cb9c924c469a10136cb
Nikolaj Hansen <nikolaj.hansen.95@gmail.com>
2017-05-22 11:27:49 +0200

    Small fix

--- NatAL/src/Program.java
+++ NatAL/src/Program.java
@@ -14,8 +14,8 @@
 
         public class Program extends JFrame
 {
-    private JTextArea Input = new JTextArea(30,20);
-    private JTextArea Output = new JTextArea(30,20);
+    private JTextArea Input = new JTextArea(25,20);
+    private JTextArea Output = new JTextArea(25,20);
     private JTextArea ErrorText = new JTextArea(10, 20);
 
     private JButton buttonCompile = new JButton("Compile");
@@ -42,6 +42,9 @@
         } );
         newPanel.add(buttonCompile, constraints);
 
+        //Input
+        Input.setText("void setup() \nend setup \n\nvoid loop()\nend loop");
+        Input.setMargin(new Insets(5,5,10,10));
         JScrollPane pane = new JScrollPane(Input);
         constraints = new GridBagConstraints();
         constraints.anchor = GridBagConstraints.NORTH;
@@ -49,11 +52,14 @@
         constraints.gridwidth = 3;
         constraints.gridx = 0;
         constraints.gridy = 0;
-        constraints.insets = new Insets(0, 0, 0, 0);
+        constraints.insets = new Insets(0, 10, 0, 0);
         constraints.weightx = 2;
+        pane.setBorder(BorderFactory.createTitledBorder(
+                BorderFactory.createEtchedBorder(), "NATaL Input"));
         newPanel.add(pane, constraints);
 
-
+        //Output
+        Output.setMargin(new Insets(5,5,10,10));
         JScrollPane pane1 = new JScrollPane(Output);
         constraints = new GridBagConstraints();
         constraints.anchor = GridBagConstraints.NORTH;
@@ -61,11 +67,14 @@
         constraints.gridwidth = 3;
         constraints.gridx = 4;
         constraints.gridy = 0;
-        constraints.insets = new Insets(0, 10, 0, 10);
+        constraints.insets = new Insets(0, 0, 0, 10);
         constraints.weightx = 2;
+        pane1.setBorder(BorderFactory.createTitledBorder(
+                BorderFactory.createEtchedBorder(), "Arduino C Output"));
         newPanel.add(pane1, constraints);
 
-
+        //Errors
+        ErrorText.setMargin(new Insets(5,5,10,10));
         JScrollPane pane2 = new JScrollPane(ErrorText);
         constraints = new GridBagConstraints();
         constraints.anchor = GridBagConstraints.NORTH;
@@ -75,6 +84,8 @@
         constraints.gridy = 10;
         constraints.insets = new Insets(10, 10, 0, 10);
         constraints.weightx = 2;
+        pane2.setBorder(BorderFactory.createTitledBorder(
+                BorderFactory.createEtchedBorder(), "Error list"));
         newPanel.add(pane2, constraints);
 
 
@@ -92,7 +103,6 @@
     public static void main(String[] args) {
                Program p = new Program();p.setVisible(true);
                 p.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
     }
     private String Compile(String input) throws IOException {
 
